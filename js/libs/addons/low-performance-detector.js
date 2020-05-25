404f8c99d71def074887ba12a1b63d9755bbdd13
katai5plate <katai5plate.sub@gmail.com>
2020-05-26 05:46:58 +0900

    add addons lpd, protector

---
+++ js/libs/addons/low-performance-detector.js
@@ -0,0 +1,69 @@
+/**
+ * システム要件に合致するかどうかを調べる関数
+ *
+ * 必須:
+ * - detect-es.js
+ * - クラス xhr-test と src が付与された script タグ
+ *
+ * システム要件:
+ * - ES8 完全対応
+ * - ES9 一部対応 (オブジェクトのspread, for-await-of)
+ * - base64 対応
+ * - WEBGL 完全対応 (experimental-webgl 禁止)
+ * - ローカルファイルアクセス
+ * - Web Audio API 対応
+ * - Fetch API 対応
+ */
+
+window.lowPerformanceDetector = function (onComplete) {
+  var reasons = [];
+  var tryit = function (reason, code) {
+    try {
+      if (typeof code === "string") {
+        new Function(code);
+        return;
+      }
+      if (!!code()) return;
+    } catch (e) {}
+    reasons.push(reason);
+  };
+  tryit("ES8", function () {
+    return detectES(8);
+  });
+  tryit("ES9 Rest/Spread Properties", "(()=>({...[1,2]}))()");
+  tryit("ES9 for-await-of", "async()=>{for await (let x of []) {}}");
+  tryit("base64 conversions", function () {
+    return "atob" in window && "btoa" in window;
+  });
+  tryit("WEBGL (Full support)", function () {
+    return document.createElement("canvas").getContext("webgl");
+  });
+  tryit("WEB Audio API", function () {
+    return "AudioContext" in window || webkitAudioContext in window;
+  });
+  tryit("Fetch API", function () {
+    return "fetch" in window;
+  });
+  var xhrResult = null;
+  try {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", document.querySelector("script.xhr-test").src);
+    xhr.overrideMimeType("text/javascript");
+    xhr.send();
+    xhr.onload = function () {
+      xhrResult = true;
+    };
+    xhr.onerror = function () {
+      xhrResult = false;
+    };
+  } catch (e) {
+    xhrResult = false;
+  }
+  var i = this.setInterval(function () {
+    if (xhrResult !== null) {
+      clearInterval(i);
+      if (xhrResult === false) reasons.push("Reading local file");
+      onComplete(reasons.length === 0, reasons);
+    }
+  }, 1);
+};
---
+++ js/libs/addons/protector.js
@@ -0,0 +1,89 @@
+/**
+ * システム要件に合致しない場合、エラー画面を出す。
+ * 合致する場合、main.js の処理を実行する。
+ *
+ * 必須:
+ * - detect-es.js
+ * - low-performance-detector
+ * - クラス xhr-test と src が付与された script タグ
+ * - js/main.js を読み込む直前
+ */
+(function () {
+  // エラー画面構築
+  var initError = document.createElement("div");
+  initError.className = "init-error";
+  var message = document.createElement("div");
+  message.className = "init-error message";
+  initError.appendChild(message);
+  document.body.appendChild(initError);
+  // main.js を一旦無力化
+  var _PluginManager = PluginManager;
+  var _SceneManager = SceneManager;
+  PluginManager = { setup: function () {} };
+  SceneManager = { run: function () {} };
+  // 処理
+  window.lowPerformanceDetector(function (result, reasons) {
+    if (result === false) {
+      document.body.style.backgroundColor = "white";
+      var chromeUrl = "https://www.google.com/intl/ja/chrome/";
+      var gotoChrome = [
+        // アツマール対応
+        "if('RPGAtsumaru' in window){window.RPGAtsumaru.popups.openLink('",
+        chromeUrl,
+        // それ以外の場合
+        "');}else{location.href='",
+        chromeUrl,
+        "';}",
+      ].join("");
+      message.innerHTML = [
+        [
+          '<h2 style="color: red;">お使いのブラウザは動作対象外です</h2>',
+          [
+            "<p>申し訳ございませんが、最新のバージョンに更新するか、",
+            '別のブラウザ ( <u style="color: blue;" onclick="' +
+              gotoChrome +
+              '">Google Chrome</u> など ) をお試しください。',
+            "また、もし以下の「エラーの原因」が「ファイルアクセス不可」のみの場合、",
+            "ブラウザの設定を変更することで改善する可能性があります。</p>",
+          ].join("<br>"),
+          "<pre><b>[ エラーの原因 ]</b>\n",
+        ],
+        reasons.map(function (x) {
+          if (x === "Reading local file") return "ファイルアクセス不可";
+          return x + " 非対応";
+        }),
+        ["</pre>"],
+      ]
+        .reduce(function (p, c) {
+          return p.concat(c);
+        }, [])
+        .join("\n");
+    } else {
+      // エラー画面を削除
+      initError.remove();
+      // main.js を再実行
+      PluginManager = _PluginManager;
+      SceneManager = _SceneManager;
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", document.querySelector('script[src~="js/main.js"]').src);
+      xhr.overrideMimeType("text/javascript");
+      xhr.send();
+      xhr.onload = function () {
+        // プラグイン設定のため実行
+        eval(xhr.response);
+        // Community_Basic 対策
+        SceneManager.preferableRendererType = function () {
+          if (!Utils.isOptionValid("canvas")) {
+            // クエリで例外的に canvas モードにできるように変更
+            return "canvas";
+          } else {
+            // Community_Basic で指定してても強制 WEBGL モード
+            return "webgl";
+          }
+        };
+        // ゲーム開始
+        window.onload();
+      };
+    }
+  });
+})();
